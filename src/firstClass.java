435da63dd44d57c84cd518ebfc615fad65d791a4
ergon77 <56317851+ergon77@users.noreply.github.com>
2019-11-09 12:59:43 -0600

    set up

---
+++ src/firstClass.java
@@ -0,0 +1,8 @@
+package gitHub;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("First Line");
+        System.out.println("Second Line");
+    }
+}

6ee4a0b71d57b1438bfab292bda98e70ea0b5c98
ergon77 <56317851+ergon77@users.noreply.github.com>
2019-11-09 13:07:06 -0600

    2nd push

---
+++ src/GroupProjects/firstClass.java
@@ -0,0 +1,7 @@
+package GroupProjects;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("Hey, hey, hey!");
+    }
+}
--- src/firstClass.java
+++
@@ -1,8 +0,0 @@
-package gitHub;
-
-public class firstClass {
-    public static void main(String[] args) {
-        System.out.println("First Line");
-        System.out.println("Second Line");
-    }
-}
